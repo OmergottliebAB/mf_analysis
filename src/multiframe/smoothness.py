c2a5bdee68a25938b8c854835157a84db60225b0
OmergottliebAB <omer.gottlieb@autobrains.ai>
2024-01-09 17:48:28 +0200

    integrating derivative logic for smoothness measurement

---
+++ src/multiframe/smoothness.py
@@ -0,0 +1,11 @@
+import logging
+import numpy as np
+import pandas as pd
+
+logger: logging.Logger = logging.getLogger("mf_analyser")
+
+def derivative_anomaly(x):
+    #TODO: implament smoothness measurement according to second derivative
+    return False
+    
+
