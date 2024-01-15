6a1184bac616c6db938f51ee9f39a709afcaffce
OmergottliebAB <omer.gottlieb@autobrains.ai>
2024-01-15 10:08:56 +0200

    todo derivative

--- src/multiframe/smoothness.py
+++ src/multiframe/smoothness.py
@@ -7,6 +7,7 @@ logger: logging.Logger = logging.getLogger("mf_analyser")
 def second_derivative_anomaly(x, age):
     # TODO: add last frame condition
     # TODO: add sign change between consecutive anomalies
+    # TODO: unconfirmed frames logic
     second_derivative = np.diff(x, n=2)
     avg, std = np.mean(second_derivative), np.std(second_derivative)
     # find indices of anomalies in second derivative and their age is larger than 10
